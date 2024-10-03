ff7d8f09aacb20124b438a8ea77fae3165ff5698
ealtamirano <ealtamirano@amr.org.ar>
2024-10-03 18:51:08 -0300

    Agrego decoder de jwt

--- alquileres-app/src/app/pages/dashboard/dashboard.page.ts
+++ alquileres-app/src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,6 @@
 // dashboard.page.ts
 import { Component, OnInit } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
@@ -14,5 +15,8 @@ export class DashboardPage implements OnInit {
 
   ngOnInit() {
     this.user = this.authService.getUser();
+    // Conseguir la informacion del user desde el token
+    const info_usuario: any = jwtDecode(localStorage.getItem('authToken')!); //Esto solo nos da el id_usuario
+    console.log(info_usuario.id_usuario);
   }
 }
