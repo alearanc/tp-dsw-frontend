3f22ba80d3b13e0c4dcc9edd679245042b486ea1
Gastón Altamirano <52184801+gastonalt@users.noreply.github.com>
2024-10-06 01:59:00 -0300

    Página para crear/editar inmuebles implementada.

--- alquileres-app/src/app/app.module.ts
+++ alquileres-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,12 +7,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomComponentsModule } from './components/custom-components.module';
+import { httpInterceptor } from './http.interceptor';
 import { CustomNavControllerService } from './services/custom-router.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, CustomComponentsModule, ReactiveFormsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, CustomNavControllerService],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    CustomNavControllerService,
+    { provide: HTTP_INTERCEPTORS, useClass: httpInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
