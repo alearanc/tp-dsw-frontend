a2c10f81d0c058f11137b5905f727e19b208597f
Gastón Altamirano <52184801+gastonalt@users.noreply.github.com>
2025-02-27 16:50:09 -0300

    Agrego funcionalidad para que se cierre el po cuando clickee una opcion

--- alquileres-app/src/app/components/header/popover-content.component.ts
+++ alquileres-app/src/app/components/header/popover-content.component.ts
@@ -1,18 +1,27 @@
 import { Component, Input } from "@angular/core";
-import { Router } from "@angular/router";
 import { PopoverController } from "@ionic/angular";
 import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
-    selector: 'app-popover-content',
-    templateUrl: './popover-content.component.html',
-    styleUrls: ['./header.component.scss'],
-  })
-  export class PopoverContentComponent {
-    @Input() userFullName: string = '';
-    @Input() signout!: () => void;
-  
-    constructor(
-      protected authService: AuthService,
-    ) {}
+  selector: 'app-popover-content',
+  templateUrl: './popover-content.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class PopoverContentComponent {
+  @Input() userFullName: string = '';
+  @Input() signout!: () => void;
+
+  constructor(
+    protected authService: AuthService,
+    private popoverController: PopoverController // Inyectamos el PopoverController
+  ) {}
+
+  closePopover() {
+    this.popoverController.dismiss();
   }
+
+  handleSignout() {
+    this.signout();
+    this.closePopover();
+  }
+}
